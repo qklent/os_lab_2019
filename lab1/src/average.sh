bf0ecf92076d55f8d7b70098432a9a5536954aa9
qklent <maksimsilenko8@gmail.com>
2024-09-24 23:43:29 +0300

    true lab1

---
+++ lab1/src/average.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+if [ "$#" -eq 0 ]; then
+    echo "Нет входных аргументов."
+    exit 1
+fi
+
+sum=0
+count=0
+
+for number in "$@"; do
+    if ! [[ "$number" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+        echo "Ошибка: '$number' не является числом."
+        exit 1
+    fi
+    sum=$(echo "$sum + $number" | bc)
+    count=$((count + 1))
+done
+
+average=$(echo "scale=2; $sum / $count" | bc)
+
+echo "Количество аргументов: $count"
+echo "Среднее арифметическое: $average"
---
+++ lab1/src/get_date.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo $(pwd)
+
+echo $(date)
+
+echo $PATH
