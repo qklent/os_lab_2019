57e944ce895212f785fe63467b2df75783e34c09
qklent <maksimsilenko8@gmail.com>
2024-10-27 13:42:46 +0300

    add revert string for lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,23 @@
 #include "revert_string.h"
+#include <string.h>
+#include <stdio.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	// two pointers on start and end, go untill len // 2 and change left and right values
+	int l = 0;
+	int r = strlen(str) - 1;
+	int mid_pointer = r / 2;
+
+    while (l < r) { 
+        // Swap characters
+        char tmp = str[l];
+        str[l] = str[r];
+        str[r] = tmp;
+
+        // Move pointers
+        l++;
+        r--;
+    }
 }
 
